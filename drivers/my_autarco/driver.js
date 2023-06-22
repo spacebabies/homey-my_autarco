84c2dc633a619cedea42c0a8d1f4e0aceb90fe77
Joost Baaij <joost@spacebabies.nl>
2023-06-22 17:46:04 +0200

    Build on the onPair handler

--- drivers/my_autarco/driver.js
+++ drivers/my_autarco/driver.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const { Driver } = require('homey');
+const Homey = require("homey");
 
-class MyAutarco extends Driver {
+class Driver extends Homey.Driver {
 
   /**
    * onInit is called when the driver is initialized.
@@ -11,26 +11,32 @@ class MyAutarco extends Driver {
     this.log('MyAutarco has been initialized');
   }
 
-  /**
-   * onPairListDevices is called when a user is adding a device
-   * and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
-   */
-  async onPairListDevices() {
-    return [
-      {
-        name: 'My inverter',
+  async onPair(session) {
+    let username, password;
+
+    // Runs after login_credentials is submitted.
+    session.setHandler("login", async (data) => {
+      username = data.username;
+      password = data.password;
+
+      return true;
+    });
+
+    session.setHandler("list_devices", async () => {
+      return {
+        name: "My inverter",
         data: {
-          id: 'my-inverter',
-        },
-        store: {
-          address: '127.0.0.1',
+          id: "kthxbai"
         },
         settings: {
-          site: "kthxbai"
+          username,
+          password
+        },
+        store: {
+          site: 'kthxbai'
         }
-      },
-    ];
+      };
+    });
   }
 
 }
